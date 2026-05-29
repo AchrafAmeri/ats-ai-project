168a968fc061216f4bbce6d4a60f61c87c844699
AchrafAmeri <ameriachraftoulouse@gmail.com>
2026-05-29 12:16:45 +0000

    feat: Ajoute une limite de taille pour l'upload. Me...

--- backend/utils/validators.py
+++ backend/utils/validators.py
@@ -6,4 +6,14 @@ def validate_pdf_file(file: UploadFile):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Seul le format PDF est accepté."
         )
+
+    file.file.seek(0, 2)
+    file_size = file.file.tell()
+    file.file.seek(0)
+
+    if file_size > 5 * 1024 * 1024:
+        raise HTTPException(
+            status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+            detail="Le fichier est trop volumineux. La limite est de 5 Mo."
+        )
     return True
