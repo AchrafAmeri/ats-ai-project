c3f471c68de8169a405a4a75ea5867049b2c8ee4
AchrafAmeri <ameriachraftoulouse@gmail.com>
2026-06-29 13:08:58 +0000

    feat: Ajoute la table des utilisateurs. Crée une no...

--- backend/db_models.py
+++ backend/db_models.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from datetime import datetime
 from backend.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
 
 class AnalysisRecord(Base):
     __tablename__ = 'analyses'
@@ -11,3 +19,4 @@ class AnalysisRecord(Base):
     score = Column(Integer)
     points_forts = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey('users.id'))
