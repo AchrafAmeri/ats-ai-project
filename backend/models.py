82b9d2165b1ef5d715592ebbec57a315831b5221
AchrafAmeri <ameriachraftoulouse@gmail.com>
2026-06-30 11:45:45 +0000

    feat: Crée les schémas Pydantic pour l'authentifica...

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel
 
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
 class JobOffer(BaseModel):
     title: str
     description: str
